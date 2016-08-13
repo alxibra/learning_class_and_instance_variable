0708c9f7509dc76325a672165814cc84f74ac12f
alexander ibrahim <alxibra@gmail.com>
2016-08-13 19:29:11 +0700

    initial commit

---
+++ class_variable.rb
@@ -0,0 +1,13 @@
+class Foo
+  @@foo_instance = 'foo_instance'
+  def self.foo_instance
+    @@foo_instance
+  end
+end
+
+class Bar < Foo
+  @@foo_instance = 'bar_instance'
+end
+
+puts Foo.foo_instance
+puts Bar.foo_instance
